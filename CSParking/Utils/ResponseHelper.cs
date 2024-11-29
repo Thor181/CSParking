68c665aac91188b8171ac5d1c48629b5642dba8c
Thor181 <art.torgaschin@yandex.ru>
2024-11-29 23:50:28 +0700

    feat: added ResponseHelper

---
+++ CSParking/Utils/ResponseHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSParking.Utils
+{
+    public static class ResponseHelper
+    {
+        public static string Format(string body)
+        {
+            return $"@{body}%";
+        }
+    }
+}
